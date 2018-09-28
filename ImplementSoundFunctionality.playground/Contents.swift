9c3398760dec144c6d8c4191a1d12c4af5517883
Banji Adewumi <banji@Banjis-Mac.local>
2018-09-28 05:18:10 -0700

    Add sound function to button

---
+++ ImplementSoundFunctionality.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: This code implements sound functionality when a button is pressed
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+    
+    var audioPlayer : AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch{
+            print(error)
+        }
+        audioPlayer.play()
+    }
+
+}
